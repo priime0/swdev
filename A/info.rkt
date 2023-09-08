a45061121899f6738d77c2eac96ba15d23eef1f4
Lucas Sta Maria <lucas@priime.dev>
2023-09-07 22:37:36 -0400

    chore: add dependency requirements for milestone A
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ A/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "xjson")
+(define deps '("base" "threading"))
+(define build-deps '("racket-doc" "rackunit-lib"))
+(define pkg-desc "Extracting JSON values")
+(define version "0.1")
+(define pkg-authors '(priime0 AndreyPiterkin))
