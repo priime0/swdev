daf5655ee65b7808541bdeb078ba8173f9a3951d
Andrey Piterkin <andrey.piterkin@gmail.com>
2023-09-18 20:31:18 -0400

    chore: create xgui base project

---
+++ B/Other/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "xgui")
+(define deps '("base" "threading" "struct-plus-plus" "htdp"))
+(define build-deps '("racket-doc" "rackunit-lib"))
+(define pkg-desc "Extracting JSON values")
+(define version "0.1")
+(define pkg-authors '(priime0 AndreyPiterkin))
