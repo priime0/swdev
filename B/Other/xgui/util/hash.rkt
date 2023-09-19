99cd723a7a8b892e7d8c910a8df651b257c70f02
Lucas Sta Maria <lucas@priime.dev>
2023-09-19 14:43:40 -0400

    test: add tests for `hash-map/make-values-symbols`
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- B/Other/xgui/util/hash.rkt
+++ B/Other/xgui/util/hash.rkt
@@ -15,3 +15,35 @@
                 (string->symbol value)
                 value)))
   (hash-map/copy ht make-value-symbol))
+
+(module+ test
+  (require rackunit)
+
+  (test-equal?
+   "empty hash"
+   (hash-map/make-values-symbols (hash))
+   (hash))
+
+  (test-equal?
+   "one key -> one string"
+   (hash-map/make-values-symbols (hash 'someKey "someString"))
+   (hash 'someKey 'someString))
+
+  (test-equal?
+   "one key -> one number"
+   (hash-map/make-values-symbols (hash 'someNumber 5))
+   (hash 'someNumber 5))
+
+  (test-equal?
+   "two keys -> string and number"
+   (hash-map/make-values-symbols (hash 'someKey "someString"
+                                       'someNumber 5))
+   (hash 'someKey 'someString
+         'someNumber 5))
+
+  (test-equal?
+   "nested hash -- check only shallow"
+   (hash-map/make-values-symbols (hash 'someKey "someString"
+                                       'someHash (hash 'notTouched "doNotTouch")))
+   (hash 'someKey 'someString
+         'someHash (hash 'notTouched "doNotTouch"))))
