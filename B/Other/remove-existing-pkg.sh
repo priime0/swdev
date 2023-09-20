6cda6446ed4afbfd179fa6a3c6d94a50605d15d7
Andrey Piterkin <andrey.piterkin@gmail.com>
2023-09-20 18:25:11 -0400

    chore: added make step to remove existing  pkg

---
+++ B/Other/remove-existing-pkg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+present=$(raco pkg show -a | grep -i "Other" | wc -l)
+
+if [ "$present" -gt 0 ]; then
+    echo "removing"
+    raco pkg remove Other
+else
+    echo "nothing to remove"
+fi
