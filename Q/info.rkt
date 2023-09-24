ebc166cf741d96d5636ae40132c9f7c3e1f308e8
Lucas Sta Maria <lucas@priime.dev>
2023-09-24 18:38:30 -0400

    chore: add `info.rkt` and missing files from B
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ Q/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "Q")
+(define deps '("base" "threading" "struct-plus-plus" "htdp"))
+(define build-deps '("racket-doc" "rackunit-lib"))
+(define pkg-desc "An implementation of the Q game")
+(define version "0.1")
+(define pkg-authors '(priime0 AndreyPiterkin))
