3a14b74dce042a2f3c6f350e2d1914a1af196cab
Lucas Sta Maria <lucas@priime.dev>
2023-09-25 22:03:52 -0400

    feat: add `singleton?` and `all-same?` list functions
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- Q/Common/util/list.rkt
+++ Q/Common/util/list.rkt
@@ -3,22 +3,37 @@
 (provide
  (contract-out
   #:unprotected-submodule no-contract
-  [member? (any/c (or/c list? any/c) . -> . boolean?)]))
+  [member? (any/c (or/c list? any/c) . -> . boolean?)]
+  [singleton? (list? . -> . boolean?)]
+  [all-same? (list? . -> . boolean?)]))
 
 #; {(X) X [Listof X] -> Boolean}
 ;; Is the given element `v` a member of the given list `lst`?
 (define (member? v lst)
   (cons? (member v lst)))
 
+#; {(X) [Listof X] -> Boolean}
+;; Is the given list a singleton? That is, is its length 1?
+(define (singleton? lst)
+  (and (pair? lst)
+       (null? (cdr lst))))
+
+#; {(X) [Listof X] -> Boolean}
+;; Are all elements in the list `equal?`
+(define (all-same? lst)
+  (or (null? lst)
+      (andmap (curry equal? (first lst))
+              (rest lst))))
+
 (module+ test
   (require rackunit)
 
   (test-equal?
-   ""
+   "x not member"
    (member? 'x '(a b c))
    #f)
 
   (test-equal?
-   ""
+   "y is member"
    (member? 'y '(x y z))
    #t))
