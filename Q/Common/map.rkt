fc980dd79f45f0746bb0a8c0c0b46a3086c141d7
Lucas Sta Maria <lucas@priime.dev>
2023-10-04 17:13:47 -0400

    fix: refactor and rename board functions
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- Q/Common/map.rkt
+++ Q/Common/map.rkt
@@ -11,22 +11,39 @@
  (contract-out
   #:unprotected-submodule no-contract
   [make-board (-> tile? board?)]
-  [board-add-tile
+  [add-tile
    (-> board?
        (cons/c integer? integer?)
        tile?
        board?)]
-  [board-possible-tile-posns
+  [valid-tile-placements
    (-> board?
        tile?
        (listof (cons/c integer? integer?)))]))
 
 
 #; {type Posn = [Pairof Integer]}
-;; A Posn is a pair of '(r . c), which represent a row and column in the Board.
+;; A Posn is a pair of '(r . c), which represent a row and column in the infinite Board.
+
+
+#; {type Direction = (U 'up 'down 'left 'right)}
+;; A Direction is a pair of '(Δr . Δc), representing a unit vector translation in one of the four
+;; distinct directions in a square grid.
+;; A Direction thus has two axes, vertical and horizontal. A Direction is a translation in only one
+;; axis. For the vertical axis, up is negative and down is positive. For the horizontal axes, left
+;; is negative and right is positive.
+(define directions
+  #hash([up    . (-1 . 0)]
+        [down  . (1 . 0)]
+        [left  . (0 . -1)]
+        [right . (0 . 1)]))
+(define direction-names (hash-keys directions))
+
+;; DEFINITION: Two `Posn`s α, β are _neighbors_ IFF WLOG there exists some direction Δ such that
+;;             `(posn-translate α Δ)` produces β.
 
 #; {Posn Direction -> Posn}
-;; Produce a new posn representing the given posn translated towards the given direction.
+;; Produce a new posn representing the given posn translated in the given direction.
 (define (posn-translate posn dir)
   (match-define (cons base-r base-c) posn)
   (match-define (cons dr dc) (hash-ref directions dir))
@@ -34,26 +51,19 @@
         (+ base-c dc)))
 
 
-#; {type Direction = (U 'up 'down 'left 'right)}
-;; A Direction represents a pair of '(δr . δc), i.e. a translation in one of the four directions
-;; in a square grid.
-(define directions
-  #hash([up    . (-1 . 0)]
-        [down  . (1 . 0)]
-        [left  . (0 . -1)]
-        [right . (0 . 1)]))
-
-
 #; {type Board = (board [HashTable Posn Tile])}
-;; A Board represents a square grid map in Q, implemented as a hash table where the keys are 2D
-;; coordinates relative to the root tile, and the values are the tiles at the given coordinate.
-;; INVARIANT: A Board is a connected graph.
-;; INVARIANT: The Tile placed at (0, 0) is the *root* tile of the game, which is the only referee
-;;            placed-tile. All other tiles have coordinates that are relative to this tile.
-;; INVARIANT: No other tile will be placed on an existing tile -- that is, two tiles cannot share
-;;            the same key/coordinates, and a tile cannot replace a tile that is already on the map.
-;; INVARIANT: A position on the board is occupied by a tile IFF the position is a key in the hash
-;;            map.
+;; A Board represents a map, implemented as a hash table where the keys are 2D coordinates relative
+;; to the root tile, and the values are the tiles at the given coordinate.
+;; DEFINITION: Two tiles are considered _adjacent_ IFF WLOG the posns where the tiles are placed are
+;;             neighbors.
+;; INVARIANT 0: A Board is a connected graph.
+;; INVARIANT 1: The Tile placed at (0, 0) is the *root* tile of the game, which is the only referee
+;;              placed-tile. All other tiles have coordinates that are relative to this tile.
+;; INVARIANT 2: No other tile will be placed on an existing tile -- that is, two tiles cannot share
+;;              the same key/coordinates, and a tile cannot replace a tile that is already on the
+;;              map.
+;; INVARIANT 3: A position on the board is occupied by a tile IFF the position is a key in the hash
+;;              map.
 (struct++ board
           ([map hash?])
           #:transparent)
@@ -68,9 +78,9 @@
 
 #; {Board Posn Tile -> Board}
 ;; Places the new tile at the given posn on the board's map.
-;; EXCEPT: Throws an error if the given position is invalid, preserving INVARIANT 0.
-(define (board-add-tile board posn new-tile)
-  (unless (board-posn-empty+has-adjacent? board posn)
+;; EXCEPT: Throws an error if the given position is invalid, preserving INVARIANT 0, 2, 3.
+(define (add-tile board posn new-tile)
+  (unless (posn-empty+has-adjacent? board posn)
     (error 'board-add-tile
            "posn ~a invalid position to place on board"
            posn))
@@ -80,59 +90,60 @@
   (set-board-map board tiles-map+))
 
 
-#; {Board Tile -> [Listof Posn]}
-;; Produce a list of valid posns where the tile can be placed in the board.
-(define (board-possible-tile-posns board tile)
+#; {Tile Board -> [Listof Posn]}
+;; Produce a list of placements (posns) that are valid for this tile on the given board.
+(define (valid-tile-placements tile board)
   (define open-posns (board-open-posns board))
-  (define valid-maybe-posn (curry board-valid-posn board tile))
-  (filter-map valid-maybe-posn open-posns))
+  (define valid-posn? (curry valid-placement? board tile))
+  (filter valid-posn? open-posns))
 
 
 #; {Board Posn -> [Maybe Tile]}
 ;; Gets the tile at `posn` if it exists, otherwise #f
-(define (board-tile-at board posn)
+(define (tile-at board posn)
   (define tiles-map (board-map board))
   ((conjoin hash-has-key? hash-ref) 
    tiles-map posn))
 
 
 #; {Board Posn -> Boolean}
-;; Is the given posn valid? That is, does the board have an empty space at the given posn, and is
-;; the given posn adjacent to any existing tile?
-(define (board-posn-empty+has-adjacent? board posn)
-  (define empty-tile-at? (negate board-tile-at))
-  ((conjoin empty-tile-at? board-posn-has-adjacent?)
+;; Does the board have an empty space at the given posn, and is the given posn adjacent to any
+;; existing tile?
+(define (posn-empty+has-adjacent? board posn)
+  (define empty-tile-at? (negate tile-at))
+  ((conjoin empty-tile-at? has-adjacent-tiles?)
    board posn))
 
 
 #; {Board Posn -> Boolean}
-;; Is the given posn adjacent to any existing tile?
+;; For the given board, does this posn have any directly adjacent tiles?
 ;; ASSUME: the given posn has no tile at its location.
-(define (board-posn-has-adjacent? board posn)
-  (define adjacent-posns (posn-adjacent/dirs posn (hash-keys directions)))
-  (define tile-at (curry board-tile-at board))
-  (define occupied-adjacent-posns (filter-map tile-at adjacent-posns))
-  (pair? occupied-adjacent-posns))
+(define (has-adjacent-tiles? board posn)
+  (define neighbor-posns          (posn-neighbors/dirs posn direction-names))
+  (define tile-at^                (curry tile-at board))
+  (define occupied-adjacent-tiles (filter-map tile-at^ neighbor-posns))
+
+  (pair? occupied-adjacent-tiles))
 
 
 #; {Posn [Listof Direction] -> [Listof Posn]}
-;; Produces the list of adjacent positions for the given posn for each direction in the given list.
+;; Produces the list of neighboring posns for the given posn for each direction in the given list.
 ;; Does not filter out duplicate positions.
-(define (posn-adjacent/dirs posn dir-list)
+(define (posn-neighbors/dirs posn dir-list)
   (define posn-translate^ (curry posn-translate posn))
   (map posn-translate^ dir-list))
 
 
 #; {Board -> [Listof Posn]}
-;; Produce a list of open positions adjacent to existing tiles.
+;; Produce a list of open positions neighboring the posns of existing tiles.
 (define (board-open-posns board)
   (define tiles-map          (board-map board))
   (define placed-tile-posns  (hash-keys tiles-map))
-  (define not-existing-tile? (curry (negate board-tile-at) board))
-  (define get-all-adjacent   (curryr posn-adjacent/dirs (hash-keys directions)))
+  (define not-existing-tile? (curry (negate tile-at) board))
+  (define get-all-neighbors  (curryr posn-neighbors/dirs (hash-keys directions)))
   (define open-posns
     (~>> placed-tile-posns
-         (map get-all-adjacent)
+         (map get-all-neighbors)
          (apply append)
          remove-duplicates
          (filter not-existing-tile?)))
@@ -147,23 +158,27 @@
    (cons tile tiles)))
 
 
-#; {Board Tile Posn -> [Maybe Posn]}
-;; Returns the given `target-posn` if it's a valid position to place the tile, otherwise `#f`.
-;; A valid Q position is one where the left and right neighbors share the same shape or (inclusive)
-;; the same color, and the up and down neighbors share the same shape or (inclusive) same color.
-(define (board-valid-posn board tile target-posn)
-  (define tile-at                   (curry board-tile-at board))
-  (define target-posn-adjacent/dirs (curry posn-adjacent/dirs target-posn))
-  (define row-neighbor-posns        (target-posn-adjacent/dirs '(left right)))
-  (define col-neighbor-posns        (target-posn-adjacent/dirs '(up down)))
-  (define row-neighbor-tiles        (filter-map tile-at row-neighbor-posns))
-  (define col-neighbor-tiles        (filter-map tile-at col-neighbor-posns))
+#; {Board Tile Posn -> Boolean}
+;; Is the given position a valid Q position to place the tile on the given board?
+;; A valid Q position is one where the left and right adjacent tiles, if any, share the same shape
+;; or (inclusive) the same color, and the up and down adjacent tiles, if any, share the same shape
+;; or (inclusive) same color.
+(define (valid-placement? board tile target-posn)
+  ;; Gets the existing tiles in the given directions.
+  (define (adjacent-tiles dirs)
+    (define tile-at^          (curry tile-at board))
+    (define target-neighbors  (curry posn-neighbors/dirs target-posn))
+    (filter-map tile-at^      (target-neighbors dirs)))
 
-  (define valid-placement?
+  (define row-adjacent-tiles (adjacent-tiles '(left right)))
+  (define col-adjacent-tiles (adjacent-tiles '(up down)))
+
+  (define adjacent-tiles? (has-adjacent-tiles? board target-posn))
+  (define matches-neighbors?
     (andmap (curry valid-tile-sequence? tile)
-            (list row-neighbor-tiles col-neighbor-tiles)))
+            (list row-adjacent-tiles col-adjacent-tiles)))
 
-  (and valid-placement? target-posn))
+  (and adjacent-tiles? matches-neighbors?))
 
 
 
@@ -189,56 +204,56 @@
 
   (test-equal?
    "board tile at (0, 0) with existing tile"
-   (board-tile-at example-board '(0 . 0))
+   (tile-at example-board '(0 . 0))
    (tile 'red 'square))
 
   (test-false
    "board tile at (-1, 0) without existing tile"
-   (board-tile-at example-board '(-1 . 0)))
+   (tile-at example-board '(-1 . 0)))
 
   (test-true
    "board tile at (1, 1) has adjacent"
-   (board-posn-has-adjacent? example-board '(1 . 1)))
+   (has-adjacent-tiles? example-board '(1 . 1)))
 
   (test-false
    "board tile at (-2, -2) does not have adjacent"
-   (board-posn-has-adjacent? example-board '(-2 . -2)))
+   (has-adjacent-tiles? example-board '(-2 . -2)))
 
   (test-equal?
    "posn adjacent dirs at (0, 0) with no directions given"
-   (posn-adjacent/dirs '(0 . 0) '())
+   (posn-neighbors/dirs '(0 . 0) '())
    '())
 
   (test-equal?
    "posn adjacent dirs at (0, 0) with every direction given"
-   (posn-adjacent/dirs '(0 . 0) '(left right up down))
+   (posn-neighbors/dirs '(0 . 0) '(left right up down))
    '((0 . -1) (0 . 1) (-1 . 0) (1 . 0)))
 
   (test-equal?
     "posn adjacent dirs at (1, 1) with some directions given"
-    (posn-adjacent/dirs '(1 . 1) '(left up down))
+    (posn-neighbors/dirs '(1 . 1) '(left up down))
     '((1 . 0) (0 . 1) (2 . 1)))
 
   (test-equal?
    "posn adjacent dirs at (1, 1) with duplicate directions given"
-   (posn-adjacent/dirs '(1 . 1) '(left left))
+   (posn-neighbors/dirs '(1 . 1) '(left left))
    '((1 . 0) (1 . 0)))
 
   (test-true
    "posn is empty and has adjacent tiles at (1, 1)"
-   (board-posn-empty+has-adjacent? example-board '(1 . 1)))
+   (posn-empty+has-adjacent? example-board '(1 . 1)))
 
   (test-false
    "posn is empty and has no adjacent tiles at (2, 2)"
-   (board-posn-empty+has-adjacent? example-board '(2 . 2)))
+   (posn-empty+has-adjacent? example-board '(2 . 2)))
 
   (test-false
    "posn is not empty and has adjacent tiles at (0, 0)"
-   (board-posn-empty+has-adjacent? example-board '(0 . 0)))
+   (posn-empty+has-adjacent? example-board '(0 . 0)))
 
   (test-equal?
    "add a tile to the board at (1, 1)"
-   (board-map (board-add-tile example-board '(1 . 1) (tile 'blue 'square)))
+   (board-map (add-tile example-board '(1 . 1) (tile 'blue 'square)))
    (hash '(0 . 0) (tile 'red 'square)
          '(1 . 0) (tile 'red 'circle)
          '(0 . 1) (tile 'green 'square)
@@ -248,12 +263,12 @@
   (test-exn
    "add a tile to the board at (0, 0) with an existing tile"
    exn:fail?
-   (thunk (board-add-tile example-board '(0 . 0) (tile 'blue 'square))))
+   (thunk (add-tile example-board '(0 . 0) (tile 'blue 'square))))
 
   (test-exn
    "add a tile to the board at (2, 2) with no adjacent tiles"
    exn:fail?
-   (thunk (board-add-tile example-board '(2 . 2) (tile 'blue 'square))))
+   (thunk (add-tile example-board '(2 . 2) (tile 'blue 'square))))
 
   (test-check
    "board open posns of example board"
@@ -293,27 +308,27 @@
 
   (test-equal?
    "board valid position for a valid tile at (1, 1)"
-   (board-valid-posn example-board (tile 'green 'circle) '(1 . 1))
+   (valid-placement? example-board (tile 'green 'circle) '(1 . 1))
    '(1 . 1))
 
   (test-false
    "board invalid position for an invalid tile at (1, 1)"
-   (board-valid-posn example-board (tile 'green 'square) '(1 . 1)))
+   (valid-placement? example-board (tile 'green 'square) '(1 . 1)))
 
   (test-check
    "possible tile positions for a red clover"
    set=?
-   (board-possible-tile-posns example-board (tile 'red 'clover))
+   (valid-tile-placements (tile 'red 'clover) example-board)
    '((0 . -1) (-1 . 0) (3 . 0) (2 . -1) (2 . 1) (1 . -1)))
 
   (test-check
    "possible tile positions for a green square"
    set=?
-   (board-possible-tile-posns example-board (tile 'green 'square))
+   (valid-tile-placements (tile 'green 'square) example-board)
    '((0 . -1) (-1 . 0) (-1 . 1) (0 . 2)))
 
   (test-check
    "no possible tile positions for a blue 8star"
    set=?
-   (board-possible-tile-posns example-board (tile 'blue '8star))
+   (valid-tile-placements (tile 'blue '8star) example-board)
    '()))
