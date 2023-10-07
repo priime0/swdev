077d58096d5651ec25a3d91b7646f72dc73a9482
Lucas Sta Maria <lucas@priime.dev>
2023-10-07 15:59:35 -0400

    feat: add `empty-space` helper function
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ Q/Common/util/image.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(require 2htdp/image)
+
+(require Q/Common/config)
+
+(provide
+ (contract-out
+  [empty-space
+   (-> (and/c real? (not/c negative?))
+       (and/c real? (not/c negative?))
+       image?)]))
+
+(define (empty-space width height)
+  (rectangle width height 'solid (*background-color*)))
