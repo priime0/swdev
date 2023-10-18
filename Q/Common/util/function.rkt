2fc4d8bb741f999072422eccc8aaad301134b099
Lucas Sta Maria <lucas@priime.dev>
2023-10-17 22:12:00 -0400

    feat: add `flip` function
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- Q/Common/util/function.rkt
+++ Q/Common/util/function.rkt
@@ -1,9 +1,16 @@
 #lang racket
 
-(provide fjoin)
+(provide
+ fjoin
+ flip)
 
 ;; Curried function that applies the given functions to the given arguments.
 (define ((fjoin . fns) . args)
   (apply values
          (map (lambda (f) (apply f args))
               fns)))
+
+#; {(X,Y,Z) (X Y -> Z) -> (Z Y -> X)}
+;; Produces a binary function that flips the arguments of a binary function.
+(define ((flip f) x y)
+  (f y x))
