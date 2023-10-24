ece92baa78f9a73a082e4d70d9fd09fbccbcaab4
Lucas Sta Maria <lucas@priime.dev>
2023-10-23 20:52:45 -0400

    feat: add `playable<%>` interface
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ Q/Common/interfaces/playable.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require racket/class)
+
+(require Q/Common/map)
+(require Q/Common/turn-info)
+(require Q/Common/data/tile)
+(require Q/Common/data/turn-action)
+
+
+(provide playable<%>)
+
+
+#; {interface Playable}
+;; Represents the public player API for interacting with a referee. Any player must implement
+;; this functionality to play a game of Q.
+(define playable<%>
+  (interface ()
+    #; {Playable -> String}
+    ;; Retrieve the name of the player
+    [name      (->m string?)]
+    #; {Playable Board Tile -> Void}
+    ;; Set up the player's knowledge of the game
+    [setup     (->m board? (listof tile?) void?)]
+    #; {Playable TurnInfo -> TurnAction}
+    ;; Execute the player's strategy with the given turn information to produce an action.
+    [take-turn (->m turn-info? turn-action?)]
+    #; {[Listof Tile] -> Void}
+    ;; Give tiles to the player
+    [new-tiles (->m (listof tile?) void?)]
+    #; {Boolean -> Void}
+    ;; Send information on whether the player won
+    [win       (->m boolean? void?)]))
