ed2333feead2d56b0eb29f1abb647b31c84ae70a
Lucas Sta Maria <lucas@priime.dev>
2023-11-02 20:10:26 -0400

    test: add `xgames`
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ 7/xgames.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(require json)
+
+(require Q/Common/game-state)
+(require Q/Player/player)
+(require Q/Referee/referee)
+
+
+(module+ main
+  (define jstate (read-json))
+  (define jactors (read-json))
+
+  (define start-state (hash->priv-state jstate))
+  (define players (map hash->player++ jactors))
+
+  (write-json (run-game players #:start-state start-state)))
