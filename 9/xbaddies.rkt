0bb7b1b9073c87ac2c557f2047b9ba57d2cf4e4c
Lucas Sta Maria <lucas@priime.dev>
2023-11-23 19:08:27 -0500

    test: add some integration tests for 9
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ 9/xbaddies.rkt
@@ -0,0 +1,47 @@
+#lang racket
+
+(require json)
+
+(require Q/Common/game-state)
+(require Q/Player/player)
+(require Q/Referee/referee)
+
+;; DELETE
+(require Q/Common/config)
+(require Q/Referee/visual-observer)
+;; DELETE
+
+(provide main)
+
+(define (main)
+  ;; DELETE
+  (define show (make-parameter #f))
+  (command-line
+   #:once-each
+   ["--show" "Attach observer to referee"
+             (show #t)])
+
+  (if (show)
+      (send (*obman*) connect (new default-observer%))
+      (void))
+  ;; DELETE
+
+  (define jstate (read-json))
+  (define jactors (read-json))
+
+  (define start-state (hash->priv-state jstate))
+  (define players (map hash->player++ jactors))
+
+  (define result (play-game players #:game-state start-state))
+  (define winners (first result))
+  (define sinners (second result))
+
+  (define sorted-winners (sort winners string<=?))
+
+  (write-json (list sorted-winners sinners))
+  (displayln "")
+  (void))
+
+(module+ main
+  (main))
+
