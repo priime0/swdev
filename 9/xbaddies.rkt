99e5158d558aa51aba009591d37408f44fe8229b
Lucas Sta Maria <lucas@priime.dev>
2023-11-23 22:56:46 -0500

    test: remove observer code in 9 test script
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- 9/xbaddies.rkt
+++ 9/xbaddies.rkt
@@ -6,28 +6,16 @@
 (require Q/Common/config)
 (require Q/Player/player)
 (require Q/Referee/referee)
-(require Q/Referee/observer)
-(require Q/Referee/visual-observer)
 (require Q/Lib/json)
 
 (provide main)
 
 (define (main)
-  (define show (make-parameter #f))
-  (command-line
-   #:once-each
-   ["--show" "Attach observer to referee"
-             (show #t)])
-
   (define jstate (read-json))
   (define jactors (read-json))
 
   (define start-state (hash->priv-state jstate))
   (define players (map hash->player++ jactors))
-
-  (if (show)
-      (send (*obman*) connect (new default-observer%))
-      (void))
 
   (define result
     (parameterize ([*bonus*        4]
