d4de8a36073fa43734cce86f458537a5bf2496dd
Lucas Sta Maria <lucas@priime.dev>
2023-11-30 21:44:56 -0500

    test: add updated xserver base
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- 10/xserver.rkt
+++ 10/xserver.rkt
@@ -1,19 +1,32 @@
 #lang racket
 
+(require Q/Common/config)
 (require Q/Server/server)
+
+(require json)
 
 (provide main)
 
 (define (main)
-  (define port (make-parameter 10000))
+  (define port
+    (command-line
+     #:args (port-num)
+     port-num))
 
-  (command-line
-   #:once-each
-   ["--port" port-str
-             "Port"
-             (port (string->number port-str))])
+  (define server-config-json (read-json))
+  (define config (hash->server-config server-config-json))
 
-  (run (port)))
+  (match-define [server-config _port tries wait signup-wait server-quiet? ref-spec] config)
+  (match-define [referee-config state0 ref-quiet? qbo fbo per-turn observe?] ref-spec)
+
+  (parameterize ([*tries*                 tries]
+                 [*signup-timeout*        wait]
+                 [*server-client-timeout* signup-wait]
+                 [*server-quiet?*         server-quiet?]
+                 [*points-per-q*          qbo]
+                 [*bonus*                 fbo]
+                 [*per-turn*              per-turn])
+    (run (port))))
 
 (module+ main
   (main))
